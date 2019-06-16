477290e05110768d9c1ef4374940a0765c534ea6
Bonkers <kubaorz1@gmail.com>
2019-06-16 10:38:30 +0200

    added input validation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,35 @@
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("=== Kalkulator BMI ===");
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj swoją wagę w kilogramach: ");
-        double weight = scanner.nextDouble();
+        double weight = getPositiveDoubleFromUser();
         System.out.println("Podaj swój wzrost w metrach: ");
-        double height = scanner.nextDouble();
+        double height = getPositiveDoubleFromUser();
 
         BMICalculator calculator = new BMICalculator(height, weight);
         System.out.println(
-                String.format("Twoje BMI to: %.2f. Wynik: %s", calculator.calculate(), calculator.interpret().toString()));
+                String.format("Twoje BMI to: %.2f. Wynik: %s", calculator.calculate(),
+                              calculator.interpret().toString()));
+    }
+
+    private static double getPositiveDoubleFromUser() {
+        String userInput = "";
+        while (true) {
+            userInput = scanner.nextLine();
+            try {
+                double input = Double.parseDouble(userInput);
+                if (input > 0) {
+                    return input;
+                } else {
+                    System.out.println("Podałeś niedodatnią liczbę, spróbuj ponownie: ");
+                }
+            } catch (NumberFormatException ex) {
+                System.out.println("Nie podałeś poprawnej liczby, spróbuj ponownie: ");
+            }
+        }
     }
 }
